878e9041d7a04b2a7873e901428f870161005309
liwenhui <38020818@qq.com>
2021-09-01 23:48:37 +0800

    init

--- ab-flink-datastream/src/main/java/com/ab/flink/transformation/TransformationApp.java
+++ ab-flink-datastream/src/main/java/com/ab/flink/transformation/TransformationApp.java
@@ -3,6 +3,7 @@ package com.ab.flink.transformation;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -18,8 +19,25 @@ public class TransformationApp {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        flatmap(env);
+        keyBy(env);
         env.execute("TransformationApp");
+    }
+
+    public static void keyBy(StreamExecutionEnvironment env) {
+        DataStreamSource<String> streamSource = env.readTextFile("data/access.log");
+        SingleOutputStreamOperator<Access> map = streamSource.map(new MapFunction<String, Access>() {
+            @Override
+            public Access map(String value) throws Exception {
+                String[] split = value.split(",");
+                Access access = new Access();
+                access.setTime(Long.parseLong(split[0]));
+                access.setDomain(split[1]);
+                access.setTraffic(Long.parseLong(split[2]));
+                return access;
+            }
+        });
+        map.keyBy(value -> value.getDomain()).sum("traffic").print();
+
     }
 
     public static void flatmap(StreamExecutionEnvironment env) {
