8a44d78e6891bcc8f6a13226a08eb7bfd73dce3d
liwenhui <38020818@qq.com>
2021-09-29 23:52:34 +0800

    study state and restart strategy

--- ab-flink-datastream/src/main/java/com/ab/flink/state/CheckpointApp.java
+++ ab-flink-datastream/src/main/java/com/ab/flink/state/CheckpointApp.java
@@ -1,7 +1,11 @@
 package com.ab.flink.state;
 
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description:
@@ -13,8 +17,14 @@ public class CheckpointApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(5000);
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.of(5, TimeUnit.SECONDS)));
         DataStreamSource<String> source = env.socketTextStream("localhost", 9527);
-        source.print();
+        source.map(value -> {
+            if ("no".equals(value)) {
+                throw new RuntimeException("发生异常，消耗一次restartAttempt次数");
+            }
+            return value.toUpperCase();
+        }).print();
         env.execute("CheckpointApp");
     }
 
