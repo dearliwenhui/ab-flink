82eae91f67ef81f5db39b19e8267ab90b514acc4
liwenhui <38020818@qq.com>
2021-10-07 20:59:34 +0800

    study checkpoint

--- ab-flink-datastream/src/main/java/com/ab/flink/state/CheckpointApp.java
+++ ab-flink-datastream/src/main/java/com/ab/flink/state/CheckpointApp.java
@@ -2,7 +2,11 @@ package com.ab.flink.state;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.concurrent.TimeUnit;
@@ -16,15 +20,29 @@ import java.util.concurrent.TimeUnit;
 public class CheckpointApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //state存储的时间
         env.enableCheckpointing(5000);
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.of(5, TimeUnit.SECONDS)));
-        DataStreamSource<String> source = env.socketTextStream("localhost", 9527);
+        CheckpointConfig config = env.getCheckpointConfig();
+        //当job是关闭时，保留checkpoint，如果不设置，当job关闭，checkpoint会被清空
+        config.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //使用配置文件设置存储路径
+        //设置state快照存储路径
+//        env.setStateBackend(new HashMapStateBackend());
+//        env.getCheckpointConfig().setCheckpointStorage("file:///checkpoint");
+
+        DataStreamSource<String> source = env.socketTextStream("106.75.171.152", 9527);
         source.map(value -> {
             if ("no".equals(value)) {
                 throw new RuntimeException("发生异常，消耗一次restartAttempt次数");
             }
             return value.toUpperCase();
-        }).print();
+        })
+
+                .map(value -> Tuple2.of(value, 1))
+                .returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(value -> value.f0).sum(1)
+                .print();
         env.execute("CheckpointApp");
     }
 
